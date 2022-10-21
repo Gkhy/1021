a6c2f2bfee1da47ae0bf998b290499d4f7919e96
wnsghd14 <wnsghd14@gmail.com>
2022-10-21 13:14:36 +0900

    add reviews_models.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Reviews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=100)
+    grade = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Reviews, on_delete=models.CASCADE)
+    content = models.TextField()
