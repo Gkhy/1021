df73f48595996829674bb78b2047fc19a3c94483
kangdh208 <kangdh208@gmail.com>
2022-10-21 14:30:55 +0900

    create_update made

---
+++ reviews/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Comment, Reviews
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Reviews
+        fields = ['title', 'content', 'movie_name', 'grade']
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment 
+        fields = ['content',]
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,10 @@
 # URL설정을 app 단위로!
 from django.urls import path 
-from . import views
-
+from .views import create_views, detail_views, index_views
 app_name = 'reviews'
 
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('', index_views.index, name='index'),
+  path('create/', create_views.create, name='create'),
+  path('<int:pk>/update/', create_views.update, name='update'),
 ]
