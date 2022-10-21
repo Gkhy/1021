2a03fe72fd3f26462863801dca4a9337995b1462
wnsghd14 <wnsghd14@gmail.com>
2022-10-21 13:38:58 +0900

    separating views.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 # from django.contrib.auth import views as auth_views
 from django.urls import path
-from . import views
-
+from .views import login_views,signup_views
 app_name = "accounts"
 urlpatterns = [
-    path("signup/", views.signup, name="signup"),
-    path('login/', views.login, name='login'),
+    # signup_views
+    path("signup/", signup_views.signup, name="signup"),
+    # login_views
+    path('login/', login_views.login, name='login'),
     # path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', login_views.logout, name='logout'),
 ]
--- accounts/views/login_views.py
+++ accounts/views/login_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from ..forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -7,17 +7,6 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
 
-def signup(request):
-    if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            auth_login(request, user)
-            return redirect("reviews:index")
-    else:
-        form = CustomUserCreationForm()
-    context = {"form": form}
-    return render(request, "accounts/signup.html", context)
 
 
 def login(request):
@@ -34,6 +23,7 @@ def login(request):
     }
     
     return render(request, 'accounts/login.html', context)
+
 
 def logout(request):
     auth_logout(request)
